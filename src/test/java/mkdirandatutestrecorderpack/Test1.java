ec89149b6a493c19e7ca40459a9d81b607b16745
gattu <gattu@DESKTOP-D8HNM4S>
2021-01-10 02:20:39 +0530

    ...

---
+++ src/test/java/mkdirandatutestrecorderpack/Test1.java
@@ -0,0 +1,84 @@
+package mkdirandatutestrecorderpack;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import atu.testrecorder.ATUTestRecorder;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 
+{
+	public static void main(String[] args) throws Exception
+	{
+		//Create a customized results folder
+		File fo=new File("1.5crore");
+		fo.mkdir();
+		//Create extent reports results file in our customized results folder
+		ExtentReports er=new ExtentReports(fo.getAbsolutePath()+"\\results.html",false); //false is for appending results
+		ExtentTest et=er.startTest("Website Title Test");
+		//Create Timestamp for dynamic video name
+		SimpleDateFormat sf=new SimpleDateFormat("dd-MMM-yyyy-hh-mm-ss");
+		Date dt=new Date();
+		String fname=sf.format(dt);
+		//Create Recording object with Path,video file and audio(false/true)
+		ATUTestRecorder rec=new ATUTestRecorder(fo.getAbsolutePath(),"videoon "+fname,true); //false is for NO Audio
+		rec.start();
+		
+		//Project oriented code
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		WebDriverWait wait=new WebDriverWait(driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q"))).sendKeys("priyaselenium1",Keys.ENTER);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='All']")));
+		Thread.sleep(2000);
+		driver.findElement(By.name("q")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.name("q")).sendKeys(Keys.chord(Keys.CONTROL,"a"),Keys.chord(Keys.CONTROL,"c"));
+		driver.navigate().to("https://www.gmail.com");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[contains(text(),'Sign in')])[2]"))).click();
+		ArrayList<String> al=new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(al.get(1));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("identifier"))).sendKeys(Keys.CONTROL,"v");
+		driver.navigate().refresh();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("identifier")));
+		String title=driver.getTitle();
+		if(title.contains("Gmail"))
+		{
+			et.log(LogStatus.PASS,"Title test passed");
+		}
+		else
+		{
+			//Screenshot
+			File src=driver.getScreenshotAs(OutputType.FILE);
+			File dest=new File(fo.getAbsolutePath()+"\\"+fname+".png");
+			FileHandler.copy(src, dest);
+			et.log(LogStatus.FAIL,"Title test failed",et.addScreenCapture(fo.getAbsolutePath()+"\\"+fname+".png"));
+		}
+		//Close site
+		driver.quit();
+		
+		//Stop recording
+		rec.stop();
+		
+		//save and close extent reports file
+		er.endTest(et);
+		er.flush();	//save
+		er.close();
+	}
+}
